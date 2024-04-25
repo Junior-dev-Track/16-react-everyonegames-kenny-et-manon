6491b1e06e8f3d3156e783f2af9670fa9b62bf17
manon <you@example.com>
2024-04-25 14:28:42 +0200

    buttons done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import HomePage from './pages/HomePage'
+
 
 function App() {
+
+    return (
+        <div>
+        <HomePage />
+        </div>
+    )
 }
 
 export default App
--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -1,15 +1,13 @@
 import Header from "../components/Header.jsx";
 import AllGames from "../components/AllGames.jsx";
-import Select from "../components/Select.jsx";
-import Filter from "../components/Filter.jsx";
+import Buttons from "../components/Buttons.jsx";
 
 
 const GamesPage = () => {
     return <div>
       <Header />
       <AllGames />
-      <Select />
-      <Filter />
+      <Buttons />
     </div>;
   }
   
