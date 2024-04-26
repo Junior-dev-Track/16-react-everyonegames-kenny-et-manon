72b76131a2baa0bf4e22984888d049d42b192f8a
KennyWouters <kenny.wouters2@gmail.com>
2024-04-26 10:46:13 +0200

    carousel wip

--- src/api/network.js
+++ src/api/network.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+
 export const http = axios.create({
-  baseURL: "ta base url de l'api example : http://localhost:8000",
+  baseURL: `https://api.rawg.io/api`,
   timeout: 20000,
 });
 
 http.defaults.headers.post["Content-Type"] = "application/json";
+
--- src/components/GameCarousel.jsx
+++ src/components/GameCarousel.jsx
@@ -1,20 +1,24 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import '../App.css';
-import '../GameCarousel.css'
+import '../GameCarousel.css';
+import axios from 'axios';
 
 const GameCarousel = () => {
     const [games, setGames] = useState([]);
+    const [centeredGameIndex, setCenteredGameIndex] = useState(0); // New state variable
 
     useEffect(() => {
     const fetchGames = async () => {
         const response = await axios.get(`https://api.rawg.io/api/games?ordering=-metacritic&page=1&key=39f531e8bfe4449383ae0f9bb9fdfb42`);
         const allGames = response.data.results;
 
-        // Limit the number of games to 8
-        const topRatedGames = allGames.slice(0, 8);
+        // Map over the games and add a new property, platformNames, which is an array of platform names
+        const topRatedGames = allGames.slice(0, 8).map(game => ({
+            ...game,
+            platformNames: game.platforms.map(platform => platform.platform.name),
+        }));
 
         setGames(topRatedGames);
     };
@@ -25,23 +29,31 @@ const GameCarousel = () => {
     return (
         <div className="carousel-container" style={{ display: 'flex', justifyContent: 'center' }}>
             <Carousel
-                showThumbs={false}
-                showIndicators={false}
-                showArrows={true}
-                centerMode
-                centerSlidePercentage={33.33}
-                infiniteLoop
-                dynamicHeight
-                // autoPlay
-                transitionTime={500}
-            >
-                {games.map((game, index) => (
-                    <div className="games-container" key={game.id}>
-                        <img className="game-image" src={game.background_image} alt={game.name} />
-                        <p className="legend">{game.name}</p>
-                    </div>
-                ))}
-            </Carousel>
+    key={games.length} // Add this line
+    showThumbs={false}
+    showIndicators={false}
+    showArrows={true}
+    centerMode
+    centerSlidePercentage={33.33}
+    infiniteLoop
+    dynamicHeight
+    // autoPlay
+    transitionTime={500}
+    onChange={setCenteredGameIndex} // Update centeredGameIndex when the centered slide changes
+>
+    {games.map((game, index) => (
+    <div className="games-container" key={game.id}>
+        <img className="game-image" src={game.background_image} alt={game.name} />
+        <div>
+            {game.platformNames.map(platformName => (
+                <p key={platformName}>{platformName}</p>
+            ))}
+        </div>
+        <p className="legend">{game.name}</p>
+        <p>{game.description}</p>
+    </div>
+))}
+</Carousel>
         </div>
     );
 };
