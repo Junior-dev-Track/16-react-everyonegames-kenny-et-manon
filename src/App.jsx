ccaf925676ca6cebcadce8d5544adfda1fc763b2
KennyWouters <kenny.wouters2@gmail.com>
2024-05-07 16:38:48 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import GamesPage from './pages/GamesPage';
 
 function App() {
     return (
-        <HomePage />
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </Router>
     );
 }
 
